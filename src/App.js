3f56dd45dc859db660363e18c574bb239236a191
Oluwatobi Akinlade <oluwatobiakinlade@Oluwatobis-MBP.lan>
2022-03-01 15:04:10 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+// const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+
+
+function App() {
+  // const [menuItems, setMenuItems] = useState(items);
+  // const [categories, setCategories] = useState(allCategories);
+
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  // const filterItems = (category) => {
+  //   if (category === 'all') {
+  //     setMenuItems(items);
+  //     return;
+  //   }
+  //   const newItems = items.filter((item) => item.category === category);
+  //   setMenuItems(newItems);
+  // };
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+
+
+
+// function App() {
+//   const [menuItems, setMenuItems] = useState(items);
+//   const [categories, setCategories] = useState(allCategories);
+
+//   const filterItems = (category) => {
+//     if(category === 'all'){
+//       setCategories(items)
+//       return;
+//     }
+//     const newItems = items.filter((item) => item.category === category)
+//     setMenuItems(newItems);
+//   }
+//   // return <main className='menu section'>
+//   //   <div className="title">
+//   //     <h2>our menu</h2>
+//   //     <div className="underline"></div>
+//   //     <Categories categories={categories} filterItems={filterItems} />
+//   //     <Menu items={menuItems} />
+//   //   </div>
+//   // </main>
+
+//   return (
+//     <main>
+//       <section className="menu section">
+//         <div className="title">
+//           <h2>our menu</h2>
+//           <div className="underline"></div>
+//         </div>
+//         <Categories categories={categories} filterItems={filterItems} />
+//         <Menu items={menuItems} />
+//       </section>
+//     </main>
+//   );
+// }
+
+
+
+export default App;
---
+++ src/Categories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Categories = ({filterItems, categories}) => {
+  
+  return <div className='btn-container'>
+    {categories.map((category, index) => {
+      return <button type="button" className='filter-btn' key={index}
+      onClick={() => filterItems(category)}>{category}</button>
+    })}
+
+  </div>;
+};
+
+// const Categories = ({ categories, filterItems }) => {
+//   return (
+//     <div className="btn-container">
+//       {categories.map((category, index) => {
+//         return (
+//           <button
+//             type="button"
+//             className="filter-btn"
+//             key={index}
+//             onClick={() => filterItems(category)}
+//           >
+//             {category}
+//           </button>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+export default Categories;
